49b6f18fd574af6f7fdb2b03cf92a23762242b2c
janakgithub <janak4u@gmail.com>
2023-09-06 20:06:24 +0930

    useSearchParams hook

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,13 +1,23 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useSearchParams} from "react-router-dom"
 
 export const Users = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showFullTimeUsers = searchParams.get('filter') ==='fullTime';
+
   return (
     <>
         <div>User Detail 1</div>
         <div>User Detail 2</div>
         <div>User Detail 3</div>
-
         <Outlet/>
+        <div>
+          <button onClick={()=>setSearchParams({filter:'fullTime'})}>Search Filter</button>
+        </div>
+        <div>
+          <button onClick={()=>setSearchParams({})}>Reset Filter</button>
+        </div>
+        {showFullTimeUsers? <h2> List of Full Time  Users</h2> : <h2> All the List of Users</h2>}
     </>
   )
 }
